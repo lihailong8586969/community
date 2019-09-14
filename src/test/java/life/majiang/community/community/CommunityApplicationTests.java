b195206e75f2bf0998d4167eb870cf7ff3ed2d8c
lihailong <2290286291@qq.com>
2019-09-14 23:32:49 +0800

    测试：测试类中添加了一个方法

--- src/test/java/life/majiang/community/community/CommunityApplicationTests.java
+++ src/test/java/life/majiang/community/community/CommunityApplicationTests.java
@@ -3,6 +3,7 @@ package life.majiang.community.community;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestComponent;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
@@ -13,4 +14,9 @@ public class CommunityApplicationTests {
     public void contextLoads() {
     }
 
+
+
+    @Test
+    public void one() {
+    }
 }
